4debb13133cd9cff3b3a694edf9d5e9093cb1e63
Jordan Veloso <jrveloso96@gmail.com>
2022-08-31 00:04:32 -0500

    fixed mobile menu color in dark mode. adjusted buttons sizes. Added nav link shrink on window size decrease before burger.

--- main.js
+++ main.js
@@ -11,9 +11,11 @@ checkbox.addEventListener('change', () => {
         document.querySelector('h1').style.color = 'rgb(147,161, 212)'
         document.querySelectorAll('.email').style.background = '#bdbddd'
         document.querySelector('button').style.border = '1.5px solid #bdbddd'
-        document.querySelector('button').classList.toggle('darkbutton:hover')
-        document.querySelector('svg').style.color = "#bdbddd"
-    } else {
+        // document.querySelector('button').classList.toggle('darkbutton:hover')
+        document.querySelector('.menu').style.backgroundColor = '#292C35'
+        document.querySelector('.menu').style.color = '#bdbddd'
+        document.querySelector('.MuiSvgIcon-root').style.fill = '#bdbddd'
+    } else if(!document.body.classList.contains('dark')) {
         document.querySelector('#headshot').src = 'images/headshot.JPG'
         document.querySelector('path').setAttribute("fill", "#292C35")
         document.querySelector('.fa-github').style.color = '#292C35'
@@ -21,6 +23,8 @@ checkbox.addEventListener('change', () => {
         document.querySelector('.fa-linkedin').style.color = 'rgb(44, 102, 188'
         document.querySelector('h1').style.color = '#292C35'
         document.querySelector('button').style.border = '1.5px solid #292C35'
+        document.querySelector('.menu').style.backgroundColor = '#fcfcfc'
+        document.querySelector('.MuiSvgIcon-root').style.fill = '#292C35'
     }
     
 })
@@ -29,11 +33,17 @@ const burger = document.querySelector('#burger')
 
 burger.addEventListener('mousedown', () => {
     document.querySelector('.menu').classList.remove('mobile-display')
+    if(document.body.classList.contains('dark')) {
+        document.querySelector('.menu').style.backgroundColor = '#292C35'
+        document.querySelector('.MuiSvgIcon-root').style.fill = '#bdbddd'
+    } else {
+        document.querySelector('.MuiSvgIcon-root').style.fill = '#292C35'
+    }
 })
 
-const body = document.querySelector('.outside')
+const x = document.querySelector('.MuiSvgIcon-root')
 
-body.addEventListener('click', () => {
+x.addEventListener('click', () => {
         document.querySelector('.menu').classList.add('mobile-display')
 })
 
