c89513b49ffc2a4291a1b2be5defd4dcf1bfbb91
Jordan Veloso <jrveloso96@gmail.com>
2022-09-14 15:19:35 -0500

    Added reveal on scroll to mobile layout.

--- main.js
+++ main.js
@@ -44,3 +44,21 @@ link.forEach( li => {
         document.querySelector('.menu').classList.add('mobile-display')
     })
 })
+
+window.addEventListener('scroll', reveal)
+
+function reveal(){
+	let reveals = document.querySelectorAll('.reveal')
+
+	for(let i = 0; i < reveals.length; i++) {
+		let windowheight = window.innerHeight
+		let revealtop = reveals[i].getBoundingClientRect().top
+		let revealpoint = 150
+
+		if(revealtop < windowheight - revealpoint) {
+			reveals[i].classList.add('active')
+		} else {
+			reveals[i].classList.remove('active')
+		}
+	}
+}
